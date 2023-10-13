0af25fa287f95d01835fe56c9c139ac89ee5312d
Samin Taheri <58706708+samin-taheri@users.noreply.github.com>
2023-10-13 13:19:06 +0300

    commiting changes

--- screens/MyWorkouts.tsx
+++ screens/MyWorkouts.tsx
@@ -1,32 +1,189 @@
-import { View, StyleSheet, Text } from "react-native";
-import { backgroundColor } from '../global';
-import CustomHeader from "../components/CustomHeader";
+import React, { useState } from 'react';
+import { View, ScrollView, Text, TouchableOpacity, Pressable, StyleSheet } from 'react-native';
+import { Calendar, LocaleConfig } from 'react-native-calendars';
+import Modal from 'react-native-modal';
+import CustomHeader from '../components/CustomHeader';
+import { Feather } from '@expo/vector-icons';
+import { cardBackground2, primaryColor } from '../global';
+import MyModal from '../components/MyModal';
 
-export default function MyWorkouts({navigation}: any) {
+LocaleConfig.locales['custom'] = {
+  monthNames: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+  monthNamesShort: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+  dayNames: ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
+  dayNamesShort: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
+};
 
-    return(
-        <View style={styles.container}>
+LocaleConfig.defaultLocale = 'custom';
+
+const DateSelector: React.FC = ({navigation}: any) => {
+  const [selectedDate, setSelectedDate] = useState<string | null>(null);
+  const [calendarVisible, setCalendarVisible] = useState(false);
+  const [currentDate, setCurrentDate] = useState<string>(getCurrentDate());
+
+  const dates = ['2023-10-13', '2023-10-14', '2023-10-15', '2023-10-16', '2023-10-17', '2023-10-18', '2023-10-19'];
+
+  function getCurrentDate(): string {
+    const currentDate = new Date();
+    return currentDate.toISOString().split('T')[0];
+  }
+
+  const handleCalendarClose = () => {
+    setCalendarVisible(false);
+  };
+
+  const handleDatePress = (date: string) => {
+    setSelectedDate(date);
+  };
+  const [isModalVisible, setModalVisible] = useState(false);
+  const toggleModal = () => {
+    setModalVisible(!isModalVisible);
+  };
+
+  const formatDateString = (dateString: string) => {
+    const date = new Date(dateString);
+    const monthName = LocaleConfig.locales.custom.monthNames[date.getMonth()];
+    const dayName = LocaleConfig.locales.custom.dayNames[date.getDay()];
+    const day = date.getDate();
+    return `${monthName} ${day} ${dayName} `;
+  };
+
+  return (
+    <View style={styles.container}>
         <CustomHeader title="My Workouts" onBack={() => navigation.goBack()} navigation={navigation}  onLogo={false}/>  
-        <View style={styles.contentContainer}>
-            <Text style={styles.text}>My Workouts</Text>
+      <Pressable
+        style={styles.currentDateButton}
+        onPress={() => setCalendarVisible(true)}
+      >
+        <Feather name="calendar" size={22} color="#000" style={{marginRight: 8}}/>
+        <Text style={styles.currentDateText}>
+        {selectedDate ? formatDateString(selectedDate) : formatDateString(currentDate)}
+        </Text>
+      </Pressable>
+      <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+        {dates.map((date) => (
+          <TouchableOpacity
+            key={date}
+            style={[styles.dateButton, selectedDate === date && styles.selectedDateButton]}
+            onPress={() => handleDatePress(date)}
+          >
+            <Text style={styles.dateText}>{formatDateString(date)}</Text>
+          </TouchableOpacity>
+          
+        ))}
+      <View style={styles.horizontalLine} /> 
+      </ScrollView>
+      {selectedDate && (
+        <View style={styles.dateScrollView}>
+        <Text style={styles.selectedDateText}>
+          {formatDateString(selectedDate)}
+        </Text>
+        <TouchableOpacity style={styles.loginButton} onPress={toggleModal}>
+          <Text style={styles.buttonText}>Create Activity</Text>
+        </TouchableOpacity>
         </View>
+      )}    
+      <Modal isVisible={calendarVisible} onBackdropPress={handleCalendarClose}>
+        <View style={styles.modalContainer}>
+          <Calendar
+            onDayPress={(day) => {
+              setSelectedDate(day.dateString);
+              handleCalendarClose();
+            }}
+            hideArrows={false}
+            hideExtraDays={true}
+            current={currentDate}
+          />
         </View>
-    )
-}
+      </Modal>
+      <MyModal isVisible={isModalVisible} onClose={toggleModal} title={''} selectedOption={null} onSelect={function (option: string): void {
+            throw new Error('Function not implemented.');
+      } } />
+    </View>
+  );
+};
+
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: backgroundColor
-    },
-    contentContainer: {
-        flex: 1,
-    },
-    text: {
-        fontSize: 18,
-        fontWeight: '600',
-        textAlign: 'left',
-        padding: 12,
-        paddingRight: '12%',
-        color: 'black', 
-    },
+  container: {
+    flex: 1,
+    justifyContent: 'space-between',
+    backgroundColor: '#fff'
+  },
+  selectedDateButton: {
+    backgroundColor: '#e1bdbc'
+  },
+  selectedDateContainer: {
+    marginLeft: 20,
+    marginTop: 20,
+  },
+  currentDateButton: {
+    padding: 10,
+    borderColor: 'black',
+    borderWidth: 1,
+    borderRadius: 5,
+    marginBottom: 10,
+    marginTop: 20,
+    alignSelf: 'flex-start',
+    marginLeft: 20,
+     width: '50%',
+     flexDirection: 'row'
+  },
+  loginButton: {
+    width: '90%',
+    height: 45,
+    backgroundColor: primaryColor,
+    borderRadius: 12,
+    justifyContent: 'center',
+    alignItems: 'center',
+    top: 40,
+    flexDirection: 'row',
+    marginLeft: 15
+  },
+  buttonText: {
+    color: 'white',
+    fontWeight: 'bold',
+  },
+  currentDateText: {
+    textAlign: 'left',
+    fontSize: 18,
+    fontWeight: '600'
+  },
+  dateButton: {
+    padding: 13,
+    borderColor: 'black',
+    borderWidth: 1,
+    borderRadius: 5,
+    margin: 8,
+    width: '9%',
+    height: '12%',
+    left: 12,
+  },
+  dateText: {
+    textAlign: 'left',
+  },
+  selectedDateText: {
+    fontSize: 22,
+    fontWeight: '600',
+    top: '30%',
+    textAlign: 'left',
+    marginLeft: 25,
+    color: '#000'
+  },
+  modalContainer: {
+    backgroundColor: 'white',
+    padding: 20,
+    borderRadius: 10,
+  },
+  horizontalLine: {
+    borderTopWidth: 2,
+    borderColor: 'gray',
+  },
+  dateScrollView: {
+    borderTopWidth: 1, 
+    borderColor: 'gray',
+    bottom: '59%',
+    marginBottom: 10,
+  },
 });
+
+export default DateSelector;
